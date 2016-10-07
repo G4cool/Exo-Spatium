00ccb93bf84c200825f72cb91354507df4ff79b7
Luca Koval <lucakoval@me.com>
2016-10-06 23:19:37 -0400

    IT WORKKS YEYEYERERH
    YEE

--- index.js
+++ index.js
@@ -149,7 +149,9 @@ function updateFrame(){
 					var originalAngle = -1 * Math.atan2((players[playerIter].mouseY - players[playerIter].windowHeight/2),(players[playerIter].mouseX - players[playerIter].windowWidth/2)) * 180 / Math.PI;
 					for (var angleShoot1 = 0; angleShoot1 < players[playerIter].numShots; angleShoot1++) {
 						//console.log("players[playerIter].numShots" + players[playerIter].numShots);
-						var startingAngle = -5 * (players[playerIter].numShots - 1);
+						console.log("numShots: " + players[playerIter].numShots);
+						//var startingAngle = -5 * (players[playerIter].numShots - 1);
+						var startingAngle = -5 * (players[playerIter].numShots - (angleShoot1 + 1));
 						var nowAngle = originalAngle + startingAngle;
 						//console.log("startingAngle: " + startingAngle);
 						slope = ((players[playerIter].windowHeight/2) - players[playerIter].mouseY)/((players[playerIter].windowWidth/2) - players[playerIter].mouseX);
