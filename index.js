f08d2fd8af51bddedacc208765e806ce3ec6c530
Luca Koval <lucakoval@me.com>
2016-11-29 21:15:03 -0500

    Blur seems to work
    yee

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ const FRAMES_PER_SECOND = 60;
 
 const SHIP_SPEED = 10;
 const TANK_ROTATION_SPEED = Math.PI * -.01;
-const BULLET_SPEED = 2;
+const BULLET_SPEED = 20;
+//const BULLET_SPEED = 2;
 
 const hitRadius = 20;
 
